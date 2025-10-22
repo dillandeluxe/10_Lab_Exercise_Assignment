653907bdb9a41924cc2ddb52d7e7701007dd2fb3
dillandeluxe <dillan.dillam@gmail.com>
2025-10-22 21:32:37 +0800

    SetUp

---
+++ ycoyg5/src/main/java/com/appdev/technominds/ycoyg5/Ycoyg5Application.java
@@ -0,0 +1,13 @@
+package com.appdev.technominds.ycoyg5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ycoyg5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ycoyg5Application.class, args);
+	}
+
+}
---
+++ ycoyg5/src/test/java/com/appdev/technominds/ycoyg5/Ycoyg5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.technominds.ycoyg5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ycoyg5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
